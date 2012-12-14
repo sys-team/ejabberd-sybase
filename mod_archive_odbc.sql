3c078b49eef42ff428fbab9d3c4275acfe1f8e47
Grigory Lopatin <g.lopatin@unact.ru>
2012-12-14 18:05:53 +0400

    add autoincrement

--- mod_archive_odbc.sql
+++ mod_archive_odbc.sql
@@ -1,4 +1,4 @@
-CREATE TABLE archive_collections ( id INT not null,
+CREATE TABLE archive_collections ( id INT default autoincrement,
                                  prev_id INTEGER,
                                  next_id INTEGER,
                                  us VARCHAR(2047) NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE archive_collections ( id INT not null,
                                  crypt INTEGER,
                                  extra VARCHAR(32767),
                                  PRIMARY KEY(id));
-CREATE TABLE archive_messages(id INTEGER NOT NULL,
+CREATE TABLE archive_messages(id INTEGER default autoincrement,
                               coll_id INTEGER NOT NULL,
                               utc DATETIME NOT NULL,
                               dir INTEGER,
@@ -38,20 +38,20 @@ CREATE TABLE archive_global_prefs(us VARCHAR(2047) NOT NULL,
                                   method_manual INTEGER,
                                   auto_save INTEGER,
                                   PRIMARY KEY(us));
-CREATE OR REPLACE TRIGGER archive_collections_update BEFORE UPDATE ON archive_collections
-referencing new as "new"
-FOR EACH ROW
-BEGIN
-  DELETE FROM archive_messages WHERE coll_id = [NEW].id;
-  UPDATE archive_collections SET prev_id = null WHERE prev_id = [NEW].id;
-  UPDATE archive_collections SET next_id = null WHERE next_id = [NEW].id;
-END;
+//CREATE OR REPLACE TRIGGER archive_collections_update BEFORE UPDATE ON archive_collections
+//referencing new as "new"
+//FOR EACH ROW
+//BEGIN
+//  DELETE FROM archive_messages WHERE coll_id = [NEW].id;
+//  UPDATE archive_collections SET prev_id = null WHERE prev_id = [NEW].id;
+//  UPDATE archive_collections SET next_id = null WHERE next_id = [NEW].id;
+//END;
 
-CREATE OR REPLACE TRIGGER archive_collections_delete BEFORE DELETE ON archive_collections
-referencing old as "old"
-FOR EACH ROW
-BEGIN
-  DELETE FROM archive_messages WHERE coll_id = [OLD].id;
-  UPDATE archive_collections SET prev_id = null WHERE prev_id = [OLD].id;
-  UPDATE archive_collections SET next_id = null WHERE next_id = [OLD].id;
-END;
+//CREATE OR REPLACE TRIGGER archive_collections_delete BEFORE DELETE ON archive_collections
+//referencing old as "old"
+//FOR EACH ROW
+//BEGIN
+//  DELETE FROM archive_messages WHERE coll_id = [OLD].id;
+//  UPDATE archive_collections SET prev_id = null WHERE prev_id = [OLD].id;
+//  UPDATE archive_collections SET next_id = null WHERE next_id = [OLD].id;
+//END;
